8644a0cb6d30c8402998c43515dc96d0f683d9ad
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2024-12-24 06:31:37 +0100

    Added "Mitgliedschaft" to FACT types

--- my_custom_tags/FactType.php
+++ my_custom_tags/FactType.php
@@ -50,6 +50,7 @@ class FactType extends AbstractElement
             'Haftstrafe'                => 'Haftstrafe',
             'Hitlerjugend'              => 'Hitlerjugend',
             'Militärdienst'             => 'Militärdienst',
+            'Mitgliedschaft'            => 'Mitgliedschaft',
         ];
     }
 }
